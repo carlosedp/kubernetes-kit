58717d84efe907c95cb3928cff591a0b8ff17007
CarlosEDP <me@carlosedp.com>
2017-05-12 15:03:48 +0000

    Added gitlab customizations

--- gitlab/install_gitlab_helm.sh
+++ gitlab/install_gitlab_helm.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-helm install --name gitlab --set image=gitlab/gitlab-ce:9.1.3-ce.0 --set externalUrl=http://gitlab.kube.local stable/gitlab-ce
+helm install --name gitlab --set image=gitlab/gitlab-ce:9.1.3-ce.0 --set serviceType=NodePort --set externalUrl=http://gitlab.kubelocal.com stable/gitlab-ce
+
 kubectl create -f gitlab-ingress.yaml

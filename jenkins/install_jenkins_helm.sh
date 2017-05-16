f0d44b7a17aa28be5a72ed141d59240a1c265fd9
CarlosEDP <me@carlosedp.com>
2017-05-16 13:14:30 +0000

    Fix configmap. Added LB to Jenkins

--- jenkins/install_jenkins_helm.sh
+++ jenkins/install_jenkins_helm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 kubectl create -f jenkins-claim.yaml
-helm install --name jenkins  --set Master.ServiceType=NodePort --set Persistence.ExistingClaim=jenkins-claim stable/jenkins
+helm install --name jenkins  --set Master.ServiceType=LoadBalancer --set Persistence.ExistingClaim=jenkins-claim stable/jenkins
 kubectl create -f jenkins-ingress.yaml

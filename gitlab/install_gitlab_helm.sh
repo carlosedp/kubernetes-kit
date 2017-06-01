524a129a2eb11d703175c50c118452c5c049fc8b
CarlosEDP <me@carlosedp.com>
2017-06-01 18:12:01 +0000

    Change ingress domain

--- gitlab/install_gitlab_helm.sh
+++ gitlab/install_gitlab_helm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-helm install --name gitlab --set image=gitlab/gitlab-ce:9.1.3-ce.0 --set serviceType=NodePort --set externalUrl=http://gitlab.kubelocal.com stable/gitlab-ce
+helm install --name gitlab --set image=gitlab/gitlab-ce:9.1.3-ce.0 --set serviceType=NodePort --set externalUrl=http://gitlab.kube.com stable/gitlab-ce
 
 kubectl create -f gitlab-ingress.yaml
